a62b5488399f38e9e821872f614150f27c6db771
iramorktum <morkt111@yandex.ru>
2022-12-27 21:27:22 +0300

    Update

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'gen_user',
-    password: 'ssfpst1ye6',
-    host: '90.156.224.18',
+    password: 'dabkuowcr3',
+    host: '90.156.227.101',
     port: 5432,
     database: 'default_db'
 });
